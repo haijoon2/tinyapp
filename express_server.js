e4fad2415ca57c21885a016d5f17dd1ca85c4cdb
Haijoon Kim <abigail.haijoon.kim@gmail.com>
2023-07-13 17:56:09 -0700

    add redirect short URLs

--- express_server.js
+++ express_server.js
@@ -49,8 +49,15 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
